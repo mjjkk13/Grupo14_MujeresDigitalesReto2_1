08799fda6fa5f4e64cbb70ea22cd281576da30d4
Ana Celedon <anaceledon488@gmail.com>
2025-10-21 19:33:47 -0500

    fetch para conectar con back

---
+++ fetch_artistas.js
@@ -0,0 +1,40 @@
+// Realizar una solicitud GET
+fetch('http://127.0.0.1/Grupo14_MujeresDigitalesReto2/registroArtistas.php?action=read', {
+    method: 'GET',
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud GET');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Datos obtenidos (GET):', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+// Realizar una solicitud POST
+fetch('http://127.0.0.1/Grupo14_MujeresDigitalesReto2/registroArtistas.php', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        nombre: 'Juan',
+        edad: 30,
+    }),
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud POST');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Respuesta del servidor (POST):', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
